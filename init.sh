065674be804e03a130c6f829985a597da212cda2
Sebastian Korzeniecki <seba5zer@gmail.com>
2023-03-25 21:22:57 +0100

    Update init script

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ docker compose run server --create &&
   sudo chown -R "${USER_UID}:0" ./server/.docker &&
   docker swarm init &&
   (cd ./server &&
+    cp swarm-dist.yml swarm.yml &&
     docker stack deploy -c swarm.yml terraria)
