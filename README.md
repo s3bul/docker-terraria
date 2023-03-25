b7f82588f3d76caa95d6191e23373326473cea08
Sebastian Korzeniecki <seba5zer@gmail.com>
2023-03-26 00:59:02 +0100

    Fix init script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # docker-terriaria
 
 https://github.com/Pryaxis/TShock/blob/v5.1.3/Dockerfile
+
+Run `./init.sh` and set wordl name `terraria`
--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -f ./.server-init ]; then
+  echo Server started
+  exit 1
+fi
+
 USER_UID=$(id -u)
 
 docker compose run server --create &&
   sudo chown -R "${USER_UID}:0" ./server/.docker &&
-  docker swarm init &&
+  chmod -R g+w ./server/.docker &&
+  (docker swarm init 2>/dev/null || echo "swarm") &&
   (cd ./server &&
     cp swarm-dist.yml swarm.yml &&
-    docker stack deploy -c swarm.yml terraria)
+    docker stack deploy -c swarm.yml terraria) &&
+  touch ./.server-init
