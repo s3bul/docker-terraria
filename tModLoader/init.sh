56f7d7550112fa83f389f57e9b8ea30d28b48d06
Sebastian Korzeniecki <seba5zer@gmail.com>
2023-03-26 21:23:17 +0200

    Fix init script

--- tModLoader/init.sh
+++ tModLoader/init.sh
@@ -15,4 +15,4 @@ chown -R "${USER_UID}:0" ./server/.docker &&
   (cd ./server &&
     cp swarm-dist.yml swarm.yml) &&
   touch ./.server-init &&
-  chown "${USER_UID}:0" ./.server-init
+  chown "${USER_UID}:0" ./.server-init ./server/swarm.yml
